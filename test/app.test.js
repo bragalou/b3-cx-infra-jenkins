d854ec15f60ae38dd3f6cdb7ff15951919f60ef4
Cyrus <74969028+Enderfire02@users.noreply.github.com>
2022-12-15 14:28:04 +0100

    Add test for Arithmetique Calculator

---
+++ test/app.test.js
@@ -0,0 +1,94 @@
+const expect = require('chai').expect;
+
+const Calculator = require('../src/app').Calculator;
+
+
+// ===== Verify Arithmetique =====
+describe('Testing Arithmetique Calculator ', function () {
+  it('Verify attribution of values', function (done) {
+    let calc1 = new Calculator(1, '+', 3)
+    expect(calc1.getValue()).deep.to.equal([1, '+', 3])
+    done()
+  })
+  it('Verify type of values/caracts', function (done) {
+    let calc1 = new Calculator(1, '+', 3)
+    expect(calc1.verifyValue()).to.equal(true)
+    done()
+  })
+  it('Verify type of values/caracts with error', function (done) {
+    let calc1 = new Calculator(1, '+', 'm')
+    let calc2 = new Calculator(1, 2, 3)
+    let calc3 = new Calculator('d', '+', 'm')
+    let calc4 = new Calculator(1, 'coucou', 3)
+    expect(calc1.verifyValue()).to.equal(false)
+    expect(calc1.calcul()).to.equal("Error Type")
+    expect(calc2.verifyValue()).to.equal(false)
+    expect(calc2.calcul()).to.equal("Error Type")
+    expect(calc3.verifyValue()).to.equal(false)
+    expect(calc3.calcul()).to.equal("Error Type")
+    expect(calc4.verifyValue()).to.equal(false)
+    expect(calc4.calcul()).to.equal("Error Type")
+    done()
+  })
+  
+  // ----- test calcul -----
+  it('Test calcul +', function (done) {
+    let calc1 = new Calculator(1, '+', 3)
+    let calc2 = new Calculator(1, '+', -3)
+    let calc3 = new Calculator(-1, '+', -3)
+    expect(calc1.calcul()).to.equal(4)
+    expect(calc2.calcul()).to.equal(-2)
+    expect(calc3.calcul()).to.equal(-4)
+    done()
+  })
+  it('Test calcul -', function (done) {
+    let calc1 = new Calculator(1, '-', 3)
+    let calc2 = new Calculator(-1, '-', 3)
+    let calc3 = new Calculator(-1, '-', -3)
+    expect(calc1.calcul()).to.equal(-2)
+    expect(calc2.calcul()).to.equal(-4)
+    expect(calc3.calcul()).to.equal(2)
+    done()
+  })
+  it('Test calcul *', function (done) {
+    let calc1 = new Calculator(2, '*', 3)
+    let calc2 = new Calculator(2, '*', -3)
+    let calc3 = new Calculator(-2, '*', -3)
+    expect(calc1.calcul()).to.equal(6)
+    expect(calc2.calcul()).to.equal(-6)
+    expect(calc3.calcul()).to.equal(6)
+    done()
+  })
+  it('Test calcul /', function (done) {
+    let calc1 = new Calculator(2, '/', 4)
+    let calc2 = new Calculator(2, '/', -4)
+    let calc3 = new Calculator(-2, '/', -4)
+    let calc4 = new Calculator(-2, '/', 4)
+    expect(calc1.calcul()).to.equal(0.5)
+    expect(calc2.calcul()).to.equal(-0.5)
+    expect(calc3.calcul()).to.equal(0.5)
+    expect(calc4.calcul()).to.equal(-0.5)
+    done()
+  })
+  it('Test calcul / by 0', function (done) {
+    let calc1 = new Calculator(2, '/', 0)
+    let calc2 = new Calculator(0, '/', 4)
+    expect(calc1.calcul()).to.equal("Error : division by 0")
+    expect(calc2.calcul()).to.equal(0)
+    done()
+  })
+  it('Test calcul % (pourcentage)', function (done) {
+    let calc1 = new Calculator(3, '%', 5)
+    let calc2 = new Calculator(6, '%', 5)
+    let calc3 = new Calculator(-3, '%', 5)
+    let calc4 = new Calculator(3, '%', -5)
+    let calc5 = new Calculator(-3, '%', -5)
+    expect(calc1.calcul()).to.equal('60%')
+    expect(calc2.calcul()).to.equal("120%")
+    expect(calc3.calcul()).to.equal("-60%")
+    expect(calc4.calcul()).to.equal("-60%")
+    expect(calc5.calcul()).to.equal("60%")
+    done()
+  })
+
+})
