7df692fbb86e1e8ccfde7be324a7ca242a45f2a8
Cyrus <74969028+Enderfire02@users.noreply.github.com>
2022-12-15 15:16:47 +0100

    Add code for Scientifique Calculator

--- src/app.js
+++ src/app.js
@@ -13,11 +13,11 @@ class Calculator {
 
 
   // ----- funtion to verify values -----
-    verifyValue() {
-      // liste caracteres autorise
-      let caracts = /([+-/%])/g
-      // verify type of number and operation
-      return typeof this.nmb1 == "number" && typeof this.nmb2 == "number" && typeof this.operation == 'string' && this.operation.match(caracts) ? true : false;
+  verifyValue() {
+    // liste caracteres autorise
+    let caracts = /([+-/*%rp])/g  // ajouter racine et puissance 
+
+    return (  (typeof this.nmb1 == "number" && typeof this.operation == 'string' && this.operation.match(caracts)) && ( (this.operation.match('r') && typeof this.nmb2 == "undefined") || (!this.operation.match('r') && typeof this.nmb2 == "number") )  )  ? true : false
   }
 
 
@@ -46,6 +46,17 @@ class Calculator {
     return Math.round(percentage).toString() + "%"
   }
 
+  puissance() {
+    return Math.round(Math.pow(this.nmb1, this.nmb2) * 100) / 100
+  }
+
+  racineCarre() {
+    if (this.nmb1 >= 0){
+      return Math.round(Math.sqrt(this.nmb1) * 100)/100
+    }
+    return "Error : cannot process negative number with square root :("    
+  }
+
 
   // ----- function to call calcul -----
   calcul() {
@@ -63,6 +74,10 @@ class Calculator {
         return this.division()
       } else if (this.operation == '%') {
         return this.pourcentage()
+      } else if (this.operation == 'r') {
+        return this.racineCarre()
+      }else if (this.operation == 'p') {
+        return this.puissance()
       }
 
     } else {
@@ -81,3 +96,8 @@ module.exports = {
 // ===== Test Execute =====
 let calc1 = new Calculator(1, '+', 3)
 console.log(calc1.calcul())
+
+let calc = new Calculator(12, 'r');
+let calc2 = new Calculator(2, 'p', 2);
+console.log(calc.calcul());
+console.log(calc2.calcul())
